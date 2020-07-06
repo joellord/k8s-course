09229eb7d69f780f44fa6ae262de291479851c41
Joel Lord <joelphy@gmail.com>
2020-07-06 10:05:26 -0400

    added a blank apiKeys file

---
+++ back/apiKeys.js
@@ -0,0 +1,5 @@
+const giphyKey = "YOUR_GIPHY_API_KEY";
+
+module.exports = {
+  "GIPHY": giphyKey
+}
--- tools/course-env.sh
+++ tools/course-env.sh
@@ -1,1 +1,4 @@
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/course -e DOCKER_WD=$(pwd) joellord/course-tools /bin/bash
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $(pwd):/home/course -e DOCKER_WD=$(pwd) \
+  -p 8080:8080 -p 3000:3000 -p 3001:3001 -p 3306:3306 \
+  joellord/course-tools /bin/bash
