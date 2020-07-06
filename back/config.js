2e7bf5323d8f0619c45b50beecb99e8d003f4e16
Joel Lord <joelphy@gmail.com>
2020-07-06 14:19:49 -0400

    Removed env variables

--- back/config.js
+++ back/config.js
@@ -1,10 +1,10 @@
 const config = {
-  MYSQL_HOST: process.env.MYSQL_HOST || "localhost",
-  MYSQL_USER: process.env.MYSQL_USER || "root",
-  MYSQL_PASS: process.env.MYSQL_PASS || "root",
-  MYSQL_DB: process.env.MYSQL_DB || "images",
-  IMAGE_PATH: process.env.IMAGE_PATH,
-  GIF_CAPTION_SVC: process.env.GIF_CAPTION_SVC || "localhost:4000"
+  MYSQL_HOST: "localhost",
+  MYSQL_USER: "root",
+  MYSQL_PASS: "root",
+  MYSQL_DB: "images",
+  IMAGE_PATH: "/tmp",
+  GIF_CAPTION_SVC: "localhost:4000"
 }
 
 module.exports = config;
